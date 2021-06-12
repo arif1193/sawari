adf5ea62619b64cf04f6fb6e550268748e00282d
arif1193 <arif1193rahman@gmail.com>
2021-06-13 02:57:12 +0600

    finish

--- src/Components/HomeDetails/HomeDetails.js
+++ src/Components/HomeDetails/HomeDetails.js
@@ -9,8 +9,8 @@ const HomeDetails = (props) => {
             <div className="view-style">
                 <div className="style">
                     <img className="img-fluid" src={data.imgUrl} alt="" />
-                    <h4>Name: {data.name}</h4>
-                    <p>Details: {data.details}</p>
+                    <h4>{data.name}</h4>
+                    <p>{data.details}</p>
                     <Link to={`/book/${data.id}`}><button className="btn-success">Book Now</button></Link>
                 </div>
             </div>
